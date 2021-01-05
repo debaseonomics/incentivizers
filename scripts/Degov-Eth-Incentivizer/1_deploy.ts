00bc6912351741808eace89bd1457e2ee45997f4
PunkUnknown <PunkUnknown@tutanota.com>
2021-01-05 23:50:34 +0500

    deploy script

--- scripts/Degov-Eth-Incentivizer/1_deploy.ts
+++ scripts/Degov-Eth-Incentivizer/1_deploy.ts
@@ -1,4 +1,4 @@
-import { parseEther } from 'ethers/lib/utils';
+import { parseEther, parseUnits } from 'ethers/lib/utils';
 import { run, ethers } from 'hardhat';
 
 import IncentivizerArtifact from '../../artifacts/contracts/Degov-Eth-Incentivizer/Incentivizer.sol/Incentivizer.json';
@@ -15,15 +15,15 @@ async function main() {
 			signer[0]
 		) as any) as IncentivizerFactory;
 
-		const rewardToken = '';
-		const pairToken = '';
-		const policy = '';
-		const rewardPercentage = 0;
-		const blockDuration = 0;
-		const enablePoolLpLimit = false;
-		const enableUserLpLimit = false;
-		const poolLpLimit = parseEther('30000');
-		const userLpLimit = parseEther('1000');
+		const rewardToken = '0x9248c485b0B80f76DA451f167A8db30F33C70907';
+		const pairToken = '0xfc835d90ea6557b57b29361d95c4584d389e6ee8';
+		const policy = '0x989Edd2e87B1706AB25b2E8d9D9480DE3Cc383eD';
+		const rewardPercentage = parseUnits('55596419',7);
+		const blockDuration = 44800;
+		const enablePoolLpLimit = true;
+		const enableUserLpLimit = true;
+		const poolLpLimit = parseEther('1000');
+		const userLpLimit = parseEther('50');
 
 		const incentivizer = await incentivizerFactory.deploy(
 			rewardToken,
